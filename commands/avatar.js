97a34a613bd16ec831215d88049f8d250f258cb3
DevFantasyYT <60735010+DevFantasyYT@users.noreply.github.com>
2020-09-04 15:03:30 +0200

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -5,13 +5,13 @@ module.exports.run = async (client, message, args) => {
     var Embed = new discord.MessageEmbed()
     if(!message.mentions.users.first()){
         Embed.setTitle("Jouw profielfoto.")
-        Embed.setThumbnail(message.author.displayAvatarURL())
+        Embed.setImage(message.author.displayAvatarURL())
         Embed.setColor("#be2ee6")
         return message.channel.send(Embed)
     }else {
         var user = message.mentions.users.first()
         Embed.setTitle(`${user.tag}'s profielfoto`)
-        Embed.setThumbnail(user.displayAvatarURL())
+        Embed.setImage(user.displayAvatarURL())
         Embed.setColor("#be2ee6")
         return message.channel.send(Embed)
     }
